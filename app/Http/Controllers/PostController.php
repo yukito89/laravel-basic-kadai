62a54280b9d98b0a80a135db2690c015a7c71300
yukito89 <yukito.yagyu@gmail.com>
2025-07-17 13:56:15 +0900

    クエリビルダ

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        $posts = DB::table('posts')->get();
+        return view('posts.index', compact('posts'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('/', function () {
 });
 
 Route::get('/posts', [PostController::class, 'index']);
+
